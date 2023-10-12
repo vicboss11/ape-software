28f3af77fac3256f668d12a1a196ddcdd91b7b87
Víctor Albán <victor.af1104@gmail.com>
2023-10-12 03:12:08 +0200

    add new values to context

--- src/context/MenuContext.jsx
+++ src/context/MenuContext.jsx
@@ -3,11 +3,14 @@ import { createContext, useState } from "react";
 export const MenuContext = createContext()
 
 export function MenuProvider({ children }) {
+  const [isOpen, setIsOpen] = useState(false)
   const [activeMenuItemId, setActiveMenuItemId] = useState()
   const [activeSubmenuItemId, setActiveSubmenuItemId] = useState()
 
   return (
     <MenuContext.Provider value={{
+      isOpen, 
+      setIsOpen,
       activeMenuItemId,
       setActiveMenuItemId,
       activeSubmenuItemId,
